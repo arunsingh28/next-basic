cc6f843ee83e25d1666644a0db34cfa2e6fa03fa
Arun Singh <arunsingh28aug.as@gmail.com>
2021-05-09 22:46:29 +0530

    fix error

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+function Home() {
+
   return (
     <div className={styles.container}>
       <Head>
@@ -67,3 +68,6 @@ export default function Home() {
     </div>
   )
 }
+
+
+export default Home
