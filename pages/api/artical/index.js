d95a798acf69e403997d04e958e0d871a7346c20
Arun Singh <arunsingh28aug.as@gmail.com>
2021-05-22 23:22:24 +0530

    clean up

--- pages/api/artical/index.js
+++
@@ -1,6 +0,0 @@
-import { articles } from "../../../data"
-
-
-export default function handle(req, res) {
-    res.status(200).json(articles)
-}
---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Artical from '../components/Artical'
+
+export default function Home({artical}) {
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+    </>
+  )
+}
+
+
